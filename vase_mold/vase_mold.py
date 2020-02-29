e33915ea32aa7ec6c54ffbe16503b1cf7ce83883
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2020-02-29 13:44:18 -0500

    made deterministic

--- vase_mold/vase_mold.py
+++ vase_mold/vase_mold.py
@@ -1,13 +1,13 @@
+import random
 from collections import namedtuple
 from math import cos, sin
-from random import random
 
 from solid import *
 from solid.utils import *
 
 from rocky_common import generate_part
 
-PHI = (1 + sqrt(5))/2
+PHI = (1 + sqrt(5)) / 2
 
 Point = namedtuple("Point", ["x", "y", "z"])
 
@@ -15,10 +15,13 @@ Point = namedtuple("Point", ["x", "y", "z"])
 def distance(p1: Point, p2: Point):
     return sqrt(abs(p1.x - p2.x) ** 1 + abs(p1.y - p2.y) ** 1)
 
+
 def argmin(iterable, key=None):
     pass
 
+
 if __name__ == '__main__':
+    random.seed("Rocky Villano 2020")
     # parameters
     num_points = 1000
     radius = 40  # mm
@@ -32,10 +35,10 @@ if __name__ == '__main__':
     point_list = []
 
     mold = 0
-    for i in range(1,num_points):
-        r = sqrt(i)*radius/sqrt(num_points)
-        h = sqrt(1 - (i/num_points)**4) * height
-        theta = i * random()
+    for i in range(1, num_points):
+        r = sqrt(i) * radius / sqrt(num_points)
+        h = sqrt(1 - (i / num_points) ** 4) * height
+        theta = random.uniform(0, 2 * pi)
         point = Point(r * cos(2 * pi * theta), r * sin(2 * pi * theta), h)
         nearest = min(point_list, key=lambda p: distance(p, point)) if point_list else point
         point_list.append(point)
@@ -46,5 +49,5 @@ if __name__ == '__main__':
             translate((nearest.x, nearest.y, 0))(sphere(r=point_radius)),
         )
 
-    mold -= translate((0,0,-radius))(cylinder(2*radius, radius))
+    mold -= translate((0, 0, -radius))(cylinder(2 * radius, radius))
     generate_part(mold, "vase_mold", 1)
