d31324ce30ab1bc8e034f6e616848d731e9b0f0c
Peter "Rocky" Villano <peter.s.villano@gmail.com>
2020-03-05 13:27:57 -0500

    now no longer self intersecting

--- vase_mold/vase_mold.py
+++ vase_mold/vase_mold.py
@@ -2,6 +2,7 @@ import random
 from collections import namedtuple
 from math import cos, sin
 
+import tqdm as tqdm
 from solid import *
 from solid.utils import *
 
@@ -29,18 +30,22 @@ if __name__ == '__main__':
     # calculated parameters
     height = radius / 4
     point_radius = spacing_factor * radius / sqrt(num_points)
-    dr = radius / num_points
-    dh = height / num_points
+    # dr = radius / num_points
+    # dh = height / num_points
 
     point_list = []
 
     mold = 0
-    for i in range(1, num_points):
+    for i in tqdm.tqdm(range(1, num_points)):
         r = sqrt(i) * radius / sqrt(num_points)
         h = sqrt(1 - (i / num_points) ** 4) * height
-        theta = random.uniform(0, 2 * pi)
-        point = Point(r * cos(2 * pi * theta), r * sin(2 * pi * theta), h)
-        nearest = min(point_list, key=lambda p: distance(p, point)) if point_list else point
+        while True:
+            theta = random.uniform(0, 2 * pi)
+            point = Point(r * cos(2 * pi * theta), r * sin(2 * pi * theta), h)
+            nearest = min(point_list, key=lambda p: distance(p, point)) if point_list else point
+            intersects_point = [distance(point,p) < 2 * point_radius for p in point_list]
+            if intersects_point.count(True) < 2:
+                break
         point_list.append(point)
         mold += hull()(
             translate(point)(sphere(r=point_radius)),
