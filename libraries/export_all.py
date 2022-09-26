73ac1dddfc6ee2a684f67c1562b25c49ec8ab10b
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-09-26 15:52:28 -0400

    bugfixes for export_all.py

--- libraries/export_all.py
+++ libraries/export_all.py
@@ -52,9 +52,10 @@ def print_err(*args, **kwargs):
     kwargs["flush"] = True
     print(*args, **kwargs)
 
-def render_parameter_set(parameter_set, args):
+
+def render_parameter_set(args, parameter_file, parameter_set):
     parameter_set: str
-    reserved_overlap = set('<>:"/\|?*').intersection(parameter_set)
+    reserved_overlap = set('<>:"/\\|?*').intersection(parameter_set)
     if reserved_overlap:
         print_err(
             f"Warning: file name contains windows reserved characters:  {''.join(reserved_overlap)} "
@@ -63,38 +64,47 @@ def render_parameter_set(parameter_set, args):
         parameter_set == reserved or parameter_set.startswith(reserved + ".")
         for reserved in WINDOWS_RESERVED_FILENAMES
     ):
-        print_err(f"Warning: filame is windows reserved: {parameter_set}")
+        print_err(f"Warning: filename is windows reserved: {parameter_set}")
 
     subprocess_args = [
         args.openscad_exe,
         args.scad_file,
         "-p",
-        args.parameter_file,
+        parameter_file,
         "-P",
         parameter_set,
         "-o",
         parameter_set + "." + args.export_format,
     ]
-    if (args.verbose): print(">>>", subprocess.list2cmdline(subprocess_args))
+    if args.verbose:
+        print(">>>", subprocess.list2cmdline(subprocess_args))
 
     if not args.dry_run:
         process = subprocess.run(subprocess_args, capture_output=True, text=True)
-        if(args.verbose and process.stdout.strip()):
-            for line in process.stdout.strip().split('\n'):
+        if args.verbose and process.stdout.strip():
+            for line in process.stdout.strip().split("\n"):
                 print("OpenSCAD:", line)
         if process.returncode != 0:
             if args.verbose:
-                print_err(f"Error: openscad returned with exit code {process.returncode}")
-            for line in process.stderr.strip().split('\n'):
+                print_err(
+                    f"Error: openscad returned with exit code {process.returncode}"
+                )
+            for line in process.stderr.strip().split("\n"):
                 print("Error (OpenSCAD):", line)
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(
-        description="Renders exports for all parameter sets for a given model and parameter file. " +
-        "Supported formats include " + ", ".join(EXPORT_CHOICES)
+        description="Renders exports for all parameter sets for a given model and parameter file. "
+        + "Supported formats include "
+        + ", ".join(EXPORT_CHOICES)
     )
-    parser.add_argument("scad_file", metavar="SCAD_FILE", type=str, help="file to render all parameterizations of")
+    parser.add_argument(
+        "scad_file",
+        metavar="SCAD_FILE",
+        type=str,
+        help="file to render each parameterization of",
+    )
     parser.add_argument(
         "parameter_files",
         nargs="*",
@@ -110,9 +120,16 @@ if __name__ == "__main__":
     )
     # todo
     # parser.add_argument("-D", help="var=val -pre-define variables")
-    parser.add_argument("--openscad-exe", metavar="EXE", default="openscad", help="alias or path to openscad exe")
+    parser.add_argument(
+        "--openscad-exe",
+        metavar="EXE",
+        default="openscad",
+        help="alias or path to openscad exe",
+    )
     parser.add_argument("--dry-run", action="store_true", help="don't create any files")
-    parser.add_argument("--verbose", action="store_true", help="explain what is being done")
+    parser.add_argument(
+        "--verbose", action="store_true", help="explain what is being done"
+    )
     args = parser.parse_args()
 
     args.scad_file: str
@@ -126,11 +143,17 @@ if __name__ == "__main__":
         try:
             with open(parameter_file, mode="r") as f:
                 parameter_set_json = json.load(f)
-                fileFormatVersion = parameter_set_json["fileFormatVersion"]
-                if not fileFormatVersion == "1":
-                    raise ValueError(f"Unsupported fileFormatVersion {fileFormatVersion}")
+                file_format_version = parameter_set_json["fileFormatVersion"]
+                if not file_format_version == "1":
+                    raise ValueError(
+                        f"Unsupported fileFormatVersion {file_format_version}"
+                    )
 
                 for parameter_set in parameter_set_json["parameterSets"].keys():
-                    render_parameter_set(parameter_set, args)
-        except FileNotFoundError as e:
+                    render_parameter_set(args, parameter_file, parameter_set)
+        except FileNotFoundError:
             print_err(f"Error: {parameter_file} not found. Continuing...")
+
+
+if __name__ == "__main__":
+    main()
