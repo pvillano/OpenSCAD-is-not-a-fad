9b9b85fa8805fd6c6e7de50cf3dabcbe2a0fa668
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-03-08 13:51:37 -0500

    add single "clip" build

--- 002 in progress/nameplates/generate_parameter_sets.py
+++ 002 in progress/nameplates/generate_parameter_sets.py
@@ -2,19 +2,24 @@ import json
 
 base = {
     "fileFormatVersion": "1",
-    "parameterSets": ...
+    "parameterSets": {
+        "000 Clip": {
+            "Fit_Width": "true",
+            "Layer": "Clip",
+            "Name": "_"
+        },
+    }
 }
-parameter_sets = dict()
+parameter_sets = base["parameterSets"]
 with open("names.txt", 'r') as f:
     for name in f:
         name = name.strip()
-        for layer in ["White", "Gold", "Blue", "Clip"]:
+        for layer in ["White", "Gold", "Blue"]:
             parameter_sets[name + ' ' + layer] = {
                 "Fit_Width": "true",
                 "Layer": layer,
                 "Name": name,
             }
-base["parameterSets"] = parameter_sets
 
 with open("nameplate.json", 'w') as f:
     json.dump(base, f, sort_keys=True, indent=4)
