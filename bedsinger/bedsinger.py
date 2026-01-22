e1f9e2c98f6d147fb386cd7b4dc24c7b656e4d65
Peter (Rocky) Villano <github@saej.in>
2026-01-22 13:34:49 -0800

    black formatting, handle corexy, comment instead of removing lines in dry run

--- bedsinger/bedsinger.py
+++ bedsinger/bedsinger.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
 import argparse
 import sys
-from collections import Counter
 from collections.abc import Iterable
-from math import sqrt, floor, log2, log
-
-
-def alpha_only(s: str) -> str:
-    return "".join(ch for ch in s if ch.isalpha())
+from math import sqrt, floor, log
 
 
 def tune(frequency: float) -> float:
-    #TODO: analyse the overtone series for a better set of notes...
     nearest_octave = 2 ** floor(log(frequency, 2))
     candidates = [
-        nearest_octave * 24/24, # first
-        nearest_octave * 27/24, # second
-        nearest_octave * 30/24, # third
-        nearest_octave * 36/24, # fifth
-        nearest_octave * 40/24, # sixth
+        nearest_octave * 24 / 24,  # first
+        nearest_octave * 27 / 24,  # second
+        nearest_octave * 30 / 24,  # third
+        nearest_octave * 36 / 24,  # fifth
+        nearest_octave * 40 / 24,  # sixth
     ]
     candidate = max([note for note in candidates if note <= frequency])
     # lowers, and by at most an octave
@@ -26,7 +20,7 @@ def tune(frequency: float) -> float:
     return candidate
 
 
-def translate(lines: Iterable[str], dry_run=True, axis: str = 'Y'):
+def translate(lines: Iterable[str], demo: bool, gantry: str):
     X = 0
     Y = 0
     Z = 0
@@ -42,9 +36,10 @@ def translate(lines: Iterable[str], dry_run=True, axis: str = 'Y'):
             continue
         comments = maybe_comments[0] if maybe_comments else ""
         op_code, *params_list = line.strip().split()
-        if dry_run and op_code in {'G80', 'M106', 'M104', 'M140', 'M109', 'M190', 'Tx', 'Tc'}:
+        if demo and op_code in "G80 M106 M104 M140 M109 M190 Tx Tc".split():
+            yield "; " + raw_line
             continue
-        if op_code not in {'G0', 'G1'}:
+        if op_code not in {"G0", "G1"}:
             yield raw_line
             continue
         params_dict = {XYZEF[0]: float(XYZEF[1:]) for XYZEF in params_list}
@@ -56,7 +51,6 @@ def translate(lines: Iterable[str], dry_run=True, axis: str = 'Y'):
         dX = nextX - X
         dY = nextY - Y
         dZ = nextZ - Z
-        # TODO: don't hardcode Y
         if dX == dY == dZ == 0:
             # we are not moving, preserve
             lastEmittedF = params_dict.get("F", lastEmittedF)
@@ -64,12 +58,14 @@ def translate(lines: Iterable[str], dry_run=True, axis: str = 'Y'):
         else:
             new_tokens = [op_code, *params_list]
             # TODO: handle arbitrary params order
-            if new_tokens[-1].startswith('F'):
+            if new_tokens[-1].startswith("F"):
                 new_tokens.pop()
 
             if dY != 0:
-                # dX^2 + dY^2 + dZ^2 = c * F^2
-                d, direction = max((abs(dX), 'X'), (abs(dY), 'Y'), (abs(dZ), 'Z'))
+                if gantry == "corexy":
+                    d, direction = max((abs(dX - dY), "X-Y"), (abs(dX + dY), "X+Y"), (abs(dZ), "Z"))
+                else:
+                    d, direction = max((abs(dX), "X"), (abs(dY), "Y"), (abs(dZ), "Z"))
                 V = sqrt((inputF ** 2) * (d ** 2) / (dX ** 2 + dY ** 2 + dZ ** 2))
                 targetV = tune(V)
 
@@ -89,52 +85,31 @@ def translate(lines: Iterable[str], dry_run=True, axis: str = 'Y'):
         Z = nextZ
 
 
-def stats(lines: Iterable[str]):
-    opcode_counter = Counter()
-    g1_axis_counter = Counter()
-    feedrate_counter = Counter()
-    for line in lines:
-        line = line.strip()
-        line, *comments = line.split(";")
-        if not line:
-            continue
-        tokens = line.split()
-        opcode_counter[tokens[0]] += 1
-        match tokens:
-            case ["G1", *rest]:
-                alpha = alpha_only(line[2:])
-                g1_axis_counter["G2" + alpha] += 1
-                for r in rest:
-                    if r.startswith("F"):
-                        feedrate_counter[float(r[1:])] += 1
-            #     case ["M204"]:
-            #         opcode_counter["M204 - Set Starting Acceleration"] += 1
-            #     case ["M73"]:
-            #         opcode_counter["M73 - Set Print Progress"] += 1
-            #     case ["G92"]:
-            #         opcode_counter["G92 - Set Position"] += 1
-            case _:
-                opcode_counter["unhandled"] += 1
-
-    print(opcode_counter.most_common(10), file=sys.stderr)
-    print(g1_axis_counter.most_common(99), file=sys.stderr)
-    print(min(feedrate_counter.items()), " ".join(sorted(map(str, feedrate_counter.most_common(3)))),
-          max(feedrate_counter.items()), file=sys.stderr)
-
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
-    parser.add_argument("axis", nargs="?", default="y")
+    parser.add_argument("file")
+
+    parser.add_argument(
+        "-d",
+        "--demo",
+        help="heating, fans, and Prusa MMU commands are commented out",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--gantry",
+        help="Cartesian printers include Ender 3, Prusa MK4/Mini, and Bambu A1/mini.  "
+             "CoreXY printers include Ender K, Prusa CORE One, and Bambu H/X/P.",
+        nargs="?",
+        default="cartesian",
+        choices=["cartesian", "corexy"],
+    )
     args = parser.parse_args()
 
     print("; Warning: bedsinger is brittle af. Use at your own risk", file=sys.stderr)
-    # with open(args.filename, encoding="utf-8") as f:
-    #     stats(f)
-    with open(args.filename, encoding="utf-8") as f:
-        for line in translate(f):
+    with open(args.file, encoding="utf-8") as f:
+        for line in translate(f, args.demo, args.gantry):
             print(line, end="")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
